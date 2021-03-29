d76451a673f9d6cc9c537df4a129fc2c1402993b
opeti <oriskopeter@gmail.com>
2021-03-29 23:51:09 +0200

    Move config.php to html folder Fix wallpapers folder name typo Move favicons outside wallpapers and use them on index.php Format some files

--- webpage/www/html/dbconfig.php
+++ webpage/www/html/dbconfig.php
@@ -1,30 +1,27 @@
 <?php
-require_once('../../config.php');
-
+require_once('config.php');
 
 class Database
-{   
-    private $host = host;
-    private $db_name = db_name;
-    private $username = username;
-    private $password = password;
-    public $conn;
-     
-    public function dbConnection()
+{
+	private $host = host;
+	private $db_name = db_name;
+	private $username = username;
+	private $password = password;
+	public $conn;
+
+	public function dbConnection()
 	{
-     
-	    $this->conn = null;    
-        try
-		{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
-        }
-		catch(PDOException $exception)
-		{
-            echo "Connection error: " . $exception->getMessage();
-        }
-         
-        return $this->conn;
-    }
+
+		$this->conn = null;
+		try {
+			$this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (PDOException $exception) {
+			echo "Connection error: " . $exception->getMessage();
+		}
+
+		return $this->conn;
+	}
 }
+
 ?>
